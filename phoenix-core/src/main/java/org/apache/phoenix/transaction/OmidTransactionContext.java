3352a0544c94878f08eb0e36b3acf898a081cba3
Ohad Shacham <ohads@yahoo-inc.com>
2017-05-10 12:31:02 +0300

    Initial setup for Omid implementation

--- phoenix-core/src/main/java/org/apache/phoenix/transaction/OmidTransactionContext.java
+++ phoenix-core/src/main/java/org/apache/phoenix/transaction/OmidTransactionContext.java
@@ -2,22 +2,65 @@ package org.apache.phoenix.transaction;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.util.concurrent.TimeoutException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
+import org.apache.phoenix.exception.SQLExceptionCode;
+import org.apache.phoenix.exception.SQLExceptionInfo;
 import org.apache.phoenix.jdbc.PhoenixEmbeddedDriver.ConnectionInfo;
 import org.apache.phoenix.schema.PTable;
 import org.apache.phoenix.util.ReadOnlyProps;
+import org.apache.tephra.TransactionFailureException;
 import org.apache.twill.zookeeper.ZKClientService;
 import org.slf4j.Logger;
+import org.apache.omid.transaction.HBaseTransactionManager;
+import org.apache.omid.transaction.RollbackException;
+import org.apache.omid.transaction.TTable;
+import org.apache.omid.transaction.Transaction;
+import org.apache.omid.transaction.TransactionException;
+import org.apache.omid.transaction.TransactionManager;
+
 
 public class OmidTransactionContext implements PhoenixTransactionContext {
 
+    private Transaction tx;
+    private TransactionManager tm;
+    
+    public OmidTransactionContext() {
+//        try (TransactionManager tm = HBaseTransactionManager.newInstance();
+//                TTable txTable = new TTable("MY_TX_TABLE")) {
+//
+//               Transaction tx = tm.begin();
+
+//               Put row1 = new Put(Bytes.toBytes("EXAMPLE_ROW1"));
+//               row1.add(family, qualifier, Bytes.toBytes("val1"));
+//               txTable.put(tx, row1);
+//
+//               Put row2 = new Put(Bytes.toBytes("EXAMPLE_ROW2"));
+//               row2.add(family, qualifier, Bytes.toBytes("val2"));
+//               txTable.put(tx, row2);
+
+//               tm.commit(tx);
+
+//           }
+    }
+
     @Override
     public void begin() throws SQLException {
-        // TODO Auto-generated method stub
+        if (tm == null) {
+            throw new SQLExceptionInfo.Builder(
+                    SQLExceptionCode.NULL_TRANSACTION_CONTEXT).build()
+                    .buildException();
+        }
 
+        try {
+            tx = tm.begin();
+        } catch (TransactionException e) {
+            throw new SQLExceptionInfo.Builder(
+                    SQLExceptionCode.TRANSACTION_FAILED)
+                    .setMessage(e.getMessage()).setRootCause(e).build()
+                    .buildException();
+        }
     }
 
     @Override
@@ -26,6 +69,7 @@ public class OmidTransactionContext implements PhoenixTransactionContext {
 
     }
 
+    
     @Override
     public void abort() throws SQLException {
         // TODO Auto-generated method stub
